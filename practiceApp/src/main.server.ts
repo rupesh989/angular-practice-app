577da83778b37a3fd4a0ab5607f7e0e0a1e30f38
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-05-27 22:47:31 +0530

    Update main.server.ts

--- practiceApp/src/main.server.ts
+++ practiceApp/src/main.server.ts
@@ -1,5 +1,1 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
 
-export default bootstrap;
